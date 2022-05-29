f7a268e415daca96f5b931d184bf8c911997ec91
infamous55 <infamouslegend555@gmail.com>
2022-05-29 15:20:31 +0300

    updated the snippet page

--- pages/snippets/[slug].tsx
+++ pages/snippets/[slug].tsx
@@ -5,6 +5,7 @@ import rehypeHighlight from 'rehype-highlight'
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
 import 'highlight.js/styles/tokyo-night-dark.css'
@@ -29,8 +30,16 @@ const SnippetPage: NextPage<{ snippet: MDXSnippet }> = ({ snippet }) => {
           href="https://cdn.jsdelivr.net/npm/katex@0.15.0/dist/katex.min.css"
         />
       </Head>
-      <h1>{snippet.meta.title}</h1>
-      <MDXRemote {...snippet.source} />
+      <Layout>
+        <div className="my-16 px-8 md:px-0">
+          <h1 className="text-4xl mb-8 font-bold text-center text-blue-200 md:text-left">
+            {snippet.meta.title}
+          </h1>
+          <div className="prose prose-lg prose-invert max-w-none text-white prose-pre:p-0 prose-pre:mb-6 prose-h1:text-3xl prose-h2:text-2xl prose-h3:text-xl prose-img:w-full">
+            <MDXRemote {...snippet.source} />
+          </div>
+        </div>
+      </Layout>
     </>
   )
 }
